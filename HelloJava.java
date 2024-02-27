32a9672683dc617faa57ae91fb8d09242e77f1b6
Corrigan Newsome <corrigannewsome@gmail.com>
2024-02-26 17:58:36 -0700

    random number

--- HelloJava.java
+++ HelloJava.java
@@ -1,6 +1,10 @@
+import java.util.Random;
+
 public class HelloJava {
 
     public static void main(String[] args) {
-        System.out.println("Hello, Java!!");
+      Random randomNum = new Random();
+      int showMe = randomNum.nextInt(100);
+      System.out.println("Random number between 1 and 100 " + "\n My random number is " + showMe);
     }
 }
